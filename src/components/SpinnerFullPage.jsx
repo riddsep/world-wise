28f956663f637f4fd750e14630ffb9ef9c30f297
riddsep <ridoseptiawan00@gmail.com>
2024-12-29 07:12:16 +0700

    finish

--- src/components/SpinnerFullPage.jsx
+++ src/components/SpinnerFullPage.jsx
@@ -2,6 +2,7 @@ import Spinner from "./Spinner";
 import styles from "./SpinnerFullPage.module.css";
 
 function SpinnerFullPage() {
+  console.log("LOADING...");
   return (
     <div className={styles.spinnerFullpage}>
       <Spinner />
